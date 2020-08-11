a919667f1f5e4409ec4a267cfd7d0071a8d8cbdb
Oscar <oscar.ramos@ucsp.edu.pe>
2020-08-10 19:45:48 -0500

    More functionalities, If room is free, Fetch all rooms even saving its results, Show all rooms available

--- scripts/schedule.utils.js
+++ scripts/schedule.utils.js
@@ -1,46 +1,79 @@
 const moment = require('moment')
+const jsonfile = require('jsonfile')
+const map = require('async/map')
+const fs = require('fs')
+
+const { fetchSchedule } = require('./fetch-schedule')
 
 const { campus, types } = require('./constants')
 
 const rooms = require('../assets/rooms.json')
 
-const { fetchSchedule } = require('./fetch-schedule')
 
 const assignBy = key => (data, item) => {
-	data[item[key]] = item;
-	return data;
+	data[item[key]] = item
+	return data
 }
 
-const normalizedRooms = rooms.reduce(assignBy("nombre"), {});
+const normalizedRooms = rooms.reduce(assignBy('nombre'), {})
 
 const getRoomData = (roomName, date) => {
-  const room = normalizedRooms[roomName]
-  return {
-    room: room['codigo'],
-    type: types[room['ambiente']],
-    campus: campus[room['campus']],
-    date
-  }
+	const room = normalizedRooms[roomName]
+	return {
+		room: room['codigo'],
+		type: types[room['ambiente']],
+		campus: campus[room['campus']],
+		date,
+	}
 }
 
 const getWeekSchedule = (roomName, date) => {
-  return fetchSchedule(getRoomData(roomName, date))
+	return fetchSchedule(getRoomData(roomName, date))
 }
 
 const isFree = (schedule, hourToCheck) => {
-  for (const event of schedule['Events']) {
-    const start = moment(new Date(event['start']))
-    const end = moment(new Date(event['end']))
-    if ((start.isBefore(hourToCheck) || start.isSame(hourToCheck)) &&
-      (end.isAfter(hourToCheck) || end.isAfter(hourToCheck))) {
-      return false
-    }
-  }
-  return true
+	for (const event of schedule['Events']) {
+		const start = moment(new Date(event['start']))
+		const end = moment(new Date(event['end']))
+		if ((start.isBefore(hourToCheck) || start.isSame(hourToCheck)) &&
+			(end.isAfter(hourToCheck) || end.isAfter(hourToCheck))) {
+			return false
+		}
+	}
+	return true
+}
+
+const fetchAndSaveAllSchedules = (day, file) =>
+	map(rooms, (room, callback) => {
+		getWeekSchedule(room.nombre, day)
+			.then(schedule => {
+				return callback(null, {
+					...schedule,
+					roomName: room.nombre
+				})
+			})
+	})
+		.then(allRoomsSchedule => {
+			const normalizedSchedules = allRoomsSchedule.reduce(assignBy('roomName'), {})
+			return jsonfile.writeFile(file, normalizedSchedules)
+		})
+
+const getSchedules = (day, file, onFetch) => {
+	if (fs.existsSync(file)) {
+		return jsonfile.readFile(file)
+	} else {
+		if (onFetch) {
+			onFetch()
+		}
+		return fetchAndSaveAllSchedules(day, file)
+			.then(() => jsonfile.readFile(file))
+	}
 }
 
 module.exports = {
-  getWeekSchedule,
-  getRoomData,
-  isFree
+	getWeekSchedule,
+	getRoomData,
+	isFree,
+	fetchAndSaveAllSchedules,
+	getSchedules,
 }
--- scripts/schedule.utils.test.js
+++ scripts/schedule.utils.test.js
@@ -1,9 +1,19 @@
 const moment = require('moment')
+const fs = require('fs')
 
-const { getRoomData, getWeekSchedule } = require('./schedule.utils')
+const {
+	getRoomData,
+	getWeekSchedule,
+	isFree,
+	fetchAndSaveAllSchedules,
+	getSchedules
+} = require('./schedule.utils')
+
 const ExampleSchedule = require('../test/ExampleSchedule')
 
 const { campus, types } = require('./constants')
+
+const rooms = require('../assets/rooms.json')
 
 
 describe('Schedule', () => {
@@ -40,3 +50,109 @@ describe('Get Room Data', () => {
 })
 
 
+describe('If an room is free by the date', () => {
+	// ExampleSchedule = await getWeekSchedule('N419  (Cap.: 24)', moment('2020-05-09', 'YYYY-MM-DD'))
+
+	test('When its too early, then all rooms are free', () => {
+		expect(isFree(ExampleSchedule, new Date('2020-05-07 6:59:00'))).toBe(true)
+	})
+
+	test('When begin an class, then the room its not occupied', () => {
+		expect(isFree(ExampleSchedule, new Date('2020-05-07 8:30:00'))).toBe(false)
+	})
+
+	it('When the class is almost over, then the room its still occupied', () => {
+		expect(isFree(ExampleSchedule, new Date('2020-05-07 10:00:00'))).toBe(false)
+	})
+})
+
+
+describe('Shows all the rooms that are available by the date', () => {
+	test('When its too early, then all rooms are available', () => {
+		const day = moment('2020-05-07', 'YYYY-MM-DD')
+		const hour = new Date('2020-05-07 6:59:00')
+		const file = `./schedules/2020-05-07.json`
+		const onFetch = () => {
+			console.log('It will fetch')
+		}
+
+		expect.assertions(1)
+		return getSchedules(day, file, onFetch)
+			.then(schedules => Object.values(schedules).filter(schedule => isFree(schedule, hour)))
+			.then(schedules => schedules.map(schedule => schedule.roomName))
+			.then(roomsAvailable => {
+				expect(roomsAvailable).toEqual(rooms.map(room => room.nombre))
+			})
+	})
+
+	test('When begin an class, then that room is not available', () => {
+		const day = moment('2020-05-07', 'YYYY-MM-DD')
+		const hour = new Date('2020-05-07 8:30:00')
+		const file = `./schedules/2020-05-07.json`
+		const onFetch = () => {
+			console.log('It will fetch')
+		}
+
+		expect.assertions(1)
+		return getSchedules(day, file, onFetch)
+			.then(schedules => Object.values(schedules).filter(schedule => isFree(schedule, hour)))
+			.then(schedules => schedules.map(schedule => schedule.roomName))
+			.then(roomsAvailable => {
+				expect(roomsAvailable.includes('N419  (Cap.: 24)')).toEqual(false)
+			})
+	})
+})
+
+describe('Fetch all rooms by the date', () => {
+	it('Should fetch and save it into a file', () => {
+		const day = moment('2020-05-09', 'YYYY-MM-DD')
+		const file = './schedules/2020-05-09.json'
+
+		expect.assertions(1)
+		return fetchAndSaveAllSchedules(day, file)
+			.then(() => {
+				expect(fs.existsSync(file)).toBe(true)
+			})
+			.catch(error => {
+				console.error(error)
+			})
+	}, 60000)
+})
+
+describe('Should get all schedules saved', () => {
+	test('When its saved, then just grab it from filesystem', () => {
+		const doesExists = '2020-05-09'
+		const day = moment(doesExists, 'YYYY-MM-DD')
+		const file = `./schedules/${doesExists}.json`
+		const onFetchMock = jest.fn(() => {
+			console.log('It will fetch')
+		})
+
+		expect.assertions(2)
+		return getSchedules(day, file, onFetchMock)
+			.then(schedule => {
+				expect(onFetchMock).not.toBeCalled()
+				expect(schedule).toEqual(require(`../schedules/${doesExists}.json`))
+			})
+	})
+
+	test('When its not saved, then fetch it', () => {
+		const doesNotExist = '2020-05-07'
+		const day = moment(doesNotExist, 'YYYY-MM-DD')
+		const file = `./schedules/${doesNotExist}.json`
+		const onFetchMock = jest.fn(() => {
+			console.log('It will fetch')
+		})
+
+		if (fs.existsSync(file)) {
+			fs.unlinkSync(file)
+		}
+
+		expect.assertions(2)
+		return getSchedules(day, file, onFetchMock)
+			.then(schedule => {
+				expect(onFetchMock).toBeCalled()
+				expect(schedule).toEqual(require(`../schedules/${doesNotExist}.json`))
+			})
+	}, 60000)
+})
